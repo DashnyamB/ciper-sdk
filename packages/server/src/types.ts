7b43d506f4e9aa955091373d14d6efbbad658ce7
dashnyamb <dashnyamkk@gmail.com>
2025-09-17 21:20:08 +0800

    fix: return role permission inside user object

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -1,5 +1,19 @@
+interface Permission {
+  id: string;
+  name: string;
+  description?: string;
+}
+
+interface Role {
+  id: string;
+  name: string;
+  identifier: string;
+  description?: string;
+  permissions: Permission[];
+}
+
 export interface CurrentUser {
   id: string;
   email: string;
-  name: string;
+  role: Role;
 }
